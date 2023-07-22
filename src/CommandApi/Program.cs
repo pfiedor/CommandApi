d92c1211bb00c7cff205ed9d6d6e874cc1a1591d
Paweł Fiedor <pawelfiedor87@gmail.com>
2023-07-22 21:06:52 +0200

    Added SqlDbContext

--- src/CommandApi/Program.cs
+++ src/CommandApi/Program.cs
@@ -1,10 +1,13 @@
 using CommandApi.Data;
+using Microsoft.EntityFrameworkCore;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddScoped<ICommandApiRepo, MockCommandApiRepo>();
+builder.Services.AddScoped<ICommandApiRepo, SqlCommandApiRepo>();
+builder.Services.AddDbContext<CommandContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSqlConnection")));
 
 var app = builder.Build();
 
