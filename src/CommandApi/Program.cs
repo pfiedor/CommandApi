9fd4f6f7f7460bae35fcb5b9560f25688058baae
Paweł Fiedor <pawelfiedor87@gmail.com>
2023-07-24 21:29:57 +0200

    Moved UserID and Password to secrets.json

--- src/CommandApi/Program.cs
+++ src/CommandApi/Program.cs
@@ -1,13 +1,21 @@
 using CommandApi.Data;
 using Microsoft.EntityFrameworkCore;
-
+using Npgsql;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddScoped<ICommandApiRepo, SqlCommandApiRepo>();
-builder.Services.AddDbContext<CommandContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSqlConnection")));
+
+var cs = new NpgsqlConnectionStringBuilder
+{
+    ConnectionString = builder.Configuration.GetConnectionString("PostgreSqlConnection"),
+    Username = builder.Configuration["UserID"],
+    Password = builder.Configuration["Password"]
+};
+
+builder.Services.AddDbContext<CommandContext>(options => options.UseNpgsql(cs.ConnectionString));
 
 var app = builder.Build();
 
